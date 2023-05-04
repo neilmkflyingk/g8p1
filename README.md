27adc4576c1629c5889926b7da86a41f2272927f
Kateryna Stetsenko <itcreativeusa@hmail.com>
2023-05-04 14:42:26 -0700

    add alerts if input empty,add message if no addres found. add icons to the list. add readme file description

--- README.md
+++ README.md
@@ -8,11 +8,16 @@ https://neilmkflyingk.github.io/house-hunters
 Github link
 https://github.com/neilmkflyingk/house-hunters
 
-This house-hunters app was created as a bootcamp student project group challange. This app is a javascript house finder. The code of this app was created from scratch. 
+This house-hunters app was created as a bootcamp student project group challenge. This app is a javascript property information finder. 
+By using geocoding, user can convert an address into geographic coordinates (latitude and longitude) that can be used to locate a place on a map. USer can then use the coordinates to display the data around that location such as nearby property information. The location will then be stored in the local storage for future reference.
+The code of this app was created from scratch. 
+
 House-hunters application uses two APIs to get search result : 
+
 1. Geocode API (https://geocode.maps.co/). Using this API, application receive latitude and longitude from the address user typed. 
 
-2. Zillow API 
+2. Attom API Using this API, application get list of properties from the latitude and longitude. 
+
 
 
 ## Table of Contents (Optional)
@@ -35,7 +40,9 @@ No special requirements
 
 1. Fill in the address & press search button
 2. Search results will appear
-3. Click any home from the list to see full infofrmation
+3. If no address entered, user will see alert message 
+4. if nothing is found according to the search criteria - user will see the warning message
+
 
 
 Screenshot located in `assets/images` folder
@@ -44,9 +51,16 @@ Screenshot located in `assets/images` folder
 
 ## Credits
 
-Geocode API (https://geocode.maps.co/)
+Geocode API https://geocode.maps.co/
+
+Attom API https://attomdata.com/
+
+Foundation CSS framework https://get.foundation/
+
+Icons https://icons8.com/
 
 Image background was taken from Unsplash website. Credits : https://unsplash.com/photos/4ojhpgKpS68
+
 
 
 ## License
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/*Declare variables*/
 var button = document.querySelector("#button");
 button.addEventListener("click", fetchHandler);
 var latitude;
@@ -8,12 +9,14 @@ var bedrooms;
 var size;
 var lot;
 var proptype;
+var listings = [];
 
+/*Function start fetch geocode API &  attomdata API on click search button */
 function fetchHandler(event) {
   event.preventDefault();
-
   var address = document.getElementById("address-search").value;
   console.log("address-input:", address);
+  /*Fetch. Get the longitude and latitude from the address entered by the user*/
   fetch("https://geocode.maps.co/search?q={" + address + "}")
     .then(function (response) {
       return response.json();
@@ -24,6 +27,7 @@ function fetchHandler(event) {
       longitude = data[0].lon;
       console.log("Latitude:", latitude);
       console.log("Longitude:", longitude);
+<<<<<<< HEAD
 
     fetch("https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/snapshot?latitude="+latitude+"&longitude="+longitude+"&radius=2", {
       headers: {'Accept': 'application/json', 'APIKey': 'b4030edd35f70d5e390f969f2e5c0f6f'}
@@ -47,28 +51,97 @@ function fetchHandler(event) {
          //create house description  
     $(".card-section").append("<p>Bathrooms: " + bathrooms + "</p>", "<p>Bedrooms: " + bedrooms + "</p>","<p>Square Footage: " + size + "</p>","<p>Lot Size (acres): " + lot + "</p>","<p>Property Type: " + proptype + "</p>",);  
       });
+=======
+      /*Fetch. Get the list of poperties from the address entered by the user*/
+      fetch(
+        "https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/snapshot?latitude=" +
+          latitude +
+          "&longitude=" +
+          longitude +
+          "&radius=2",
+        {
+          headers: {
+            Accept: "application/json",
+            APIKey: "b4030edd35f70d5e390f969f2e5c0f6f",
+          },
+        }
+      )
+        .then(function (response) {
+          if (response.status !== 200) {
+            /*If response.status is not 200 view this message.*/
+            $(".card-section").append(
+              "<h5>No addres was found with your search criteria. Try another addres!</h5>"
+            );
+          }
+          return response.json();
+        })
+        .then(function (data) {
+          console.log("attom-response:", data);
+          console.log("address: ", street);
+          data.property.forEach((element) => {
+            street = element.address.oneLine;
+            bathrooms = element.building.rooms.bathstotal;
+            bedrooms = element.building.rooms.beds;
+            size = element.building.size.universalsize;
+            lot = element.lot.lotSize1;
+            proptype = element.summary.propclass;
+            /* Create title & description of each house*/
+
+            /* Create ul & append to the page*/
+            $(".card-section").append("<ul class='list'>");
+            /* Append h4 & li list inside ul*/
+            $(".list").append(
+              /* Create title h4 & append to the page*/
+              "<h4>" + street + "</h4>",
+              /* Create list of li  & append to the page*/
+              "<li><img src='https://img.icons8.com/ios/20/null/bath.png'/> Bathrooms: " +
+                bathrooms +
+                "</li>",
+              "<li> <img src='https://img.icons8.com/ios/20/null/bedroom.png'/> Bedrooms: " +
+                bedrooms +
+                "</li>",
+              "<li><img src='https://img.icons8.com/external-outline-black-m-oki-orlando/20/null/external-squares-math-vol-2-outline-outline-black-m-oki-orlando.png'/> Square Footage: " +
+                size +
+                "</li>",
+              "<li><img src='https://img.icons8.com/ios/20/null/screen-resolution.png'/> Lot Size (acres): " +
+                lot +
+                "</li>",
+              "<li><img src='https://img.icons8.com/ios/20/null/home--v1.png'/> Property Type: " +
+                proptype +
+                "</li>"
+            );
+          });
+        });
+>>>>>>> 9bcf60f (add alerts if input empty,add message if no addres found. add icons to the list. add readme file description)
     });
   });
 }
-var listings = [];
 
-//Function show results on click or alert if empty
+/* Function show results on click or alert if empty*/
 $(document).ready(function () {
   $("button").click(function () {
     if (!$("#address-search").val()) {
       alert("Enter Street Address, City and Zip Code!");
-    }
-    else {
+    } else {
       $(".result").removeClass("hidden");
     }
 
+<<<<<<< HEAD
 
+=======
+    // function init() {
+    // var storedListings = JSON.parse(localStorage.getItem("listing"));
+    // if (storedListings != null) {
+    //   listings = storedListings;
+    // }}
+>>>>>>> 9bcf60f (add alerts if input empty,add message if no addres found. add icons to the list. add readme file description)
 
     var listing = (document.getElementById("address-search").value);
     localStorage.setItem("listing", listing);
   });
 });
 
+<<<<<<< HEAD
 // create & add elements using jquery
   var resultCardBlock = "<div class='result-card'></div>";  
   $(".result").append(resultCardBlock);  
@@ -90,3 +163,17 @@ $(document).ready(function () {
     var saveAddress = localStorage.getItem('listing');
     input.value = saveAddress
   }
+=======
+/* Create & add elements using jquery*/
+var resultCardBlock = "<div class='result-card'></div>";
+$(".result").append(resultCardBlock);
+var resultCardContent =
+  "<div class='grid-x grid-margin-x result-card-content'></div>";
+$(".result-card").append(resultCardContent);
+var resultCardTitle = "<div class='card-section'></div>";
+$(".result-card-content").append(resultCardTitle);
+/*Create footer with github link*/
+var footer =
+  "<footer class='text-center'><a href='https://github.com/neilmkflyingk/house-hunters'><i class='fi-social-github'>HOUSE HUNTERS</i></a></footer>";
+$(".result").append(footer);
+>>>>>>> 9bcf60f (add alerts if input empty,add message if no addres found. add icons to the list. add readme file description)
